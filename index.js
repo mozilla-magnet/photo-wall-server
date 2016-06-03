e04e2ec5d866349020c9c6cd70ab553f35cee535
Sam Giles <sam.e.giles@gmail.com>
2016-06-03 18:14:03 +0100

    Set up routes and eslint

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const config = require('./config.json');
+const app = require('./lib/app');
+
+const port = process.env.PORT || config.default_port;
+const address = process.env.ADDRESS || config.address || '';
+const server = app.listen(port, address, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log(`photo wall service listening at http://${host}:${port}`);
+});
